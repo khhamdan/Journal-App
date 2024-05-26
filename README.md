86a7c61456a280cade4e20478484deba6e4d05d4
Hamdan <hamdanahmadkhawaja@gmail.com>
2024-05-26 18:39:59 +0500

    Added Lombok

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 This repository is for the journal app backend built in Spring Boot with MongoDb 
 1. CRUD API are implemented
 2. HTTP status code are implemented
+3. Added Lombok
--- src/main/java/com/hamdan/journalapp/entity/JournalEntries.java
+++ src/main/java/com/hamdan/journalapp/entity/JournalEntries.java
@@ -1,6 +1,8 @@
 package com.hamdan.journalapp.entity;
 
 
+import lombok.Getter;
+import lombok.Setter;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -9,43 +11,14 @@ import java.time.LocalDateTime;
 import java.util.Date;
 
 @Document("journal_entries")
+@Setter
+@Getter
 public class JournalEntries
 {
     @Id
     private ObjectId id;
     private String title;
     private String content;
-
     private LocalDateTime date;
 
-    public LocalDateTime getDate() {
-        return date;
-    }
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
-
-    public ObjectId getId() {
-        return id;
-    }
-
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
 }
